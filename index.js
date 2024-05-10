18bc8116fc3c095939143b6fbceddeada1eb6e4b
prajwal <prajwalshah912@gmail.com>
2024-05-10 16:51:30 +0530

    duplicated the child node and added the attribute aria-hidden

--- index.js
+++ index.js
@@ -7,5 +7,15 @@ if(!window.matchMedia("(prefers-reduced-motion:reduce)").matches){
 function addAnimation(){
     scrollers.forEach((scroller) => {
         scroller.setAttribute('data-animated', true);
-    })
+
+        const scrollerInner = scroller.querySelector(".scroller__inner");
+        const scrollContent = Array.from(scrollerInner.inner);
+
+        scrollContent.foreach((item) => {
+            const duplicatedItem = item.cloneNode(true);
+            duplicatedItem.setAttribute("aria-hidden", true);
+        })
+
+    });
+
 }
